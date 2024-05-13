b578cb42854335ef72ec29c72771905b8cd562f3
Yannick Priol <yannickpriol@orange.fr>
2024-05-13 13:02:48 +0200

    error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,8 +1,16 @@
-
-
+import '../../styles/Error.css'
+import { Link } from 'react-router-dom';
 
 function Error() {
-    return <div>Erreur 🏡</div>
-}
 
-export default Error;
+    return (
+      <div className='kasa-error'>
+        <div className='alert-404'>404</div>
+        <h2>Oups ! La page que vous demandez n'existe pas.</h2>
+        <Link to="/" className="link">Retourner sur la page d'accueil</Link>
+        
+      </div>
+    )
+  }
+  
+  export default Error
