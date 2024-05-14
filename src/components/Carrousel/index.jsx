eec92a470131763a27f4f127a7038e6b61ff9b15
Yannick Priol <yannickpriol@orange.fr>
2024-05-14 15:29:24 +0200

    carrousel sans animation

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -1,10 +1,35 @@
 import '../../styles/Carrousel.css';
-
+import arrow_left from '../../assets/arrow_left.png';
+import arrow_right from '../../assets/arrow_right.png';
+import { useState } from 'react';
 
 function Carrousel({ pictures }) {
 
+    const [activePicture, setActivePicture] = useState(0);
+
+    const handleLeftArrowClick = () => {
+        activePicture === 0 ? setActivePicture(pictures.length-1) : setActivePicture(activePicture-1);
+    };
+    
+    const handleRightArrowClick = () => {
+        activePicture === pictures.length-1 ? setActivePicture(0) : setActivePicture(activePicture+1);
+    };
+
+   
     return (
-            <div className='kasa-carrousel'>
+        <div className='kasa-carrousel'>
+            <img src={pictures[activePicture]} alt={'appartement '+activePicture+1} />
+            { pictures.length > 1 && (
+                <div className='kasa-carrousel-dynamics'>
+                    <div className='kasa-carrousel-arrow-left-container' onClick={handleLeftArrowClick}>
+                        <img src={arrow_left} alt='' />
+                    </div>
+                    <span className='kasa-carrousel-count'>{activePicture+1}/{pictures.length}</span>
+                    <div className='kasa-carrousel-arrow-right-container' onClick={handleRightArrowClick}>
+                        <img src={arrow_right} alt='' />
+                    </div>
+                </div>
+            ) }
         </div>
     );
 }
