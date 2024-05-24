24eafe0573ef5f4bee6eb24a1f74da44c0614dbd
Yannick Priol <yannickpriol@orange.fr>
2024-05-24 13:54:51 +0200

    documentation Carrousel

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -3,20 +3,26 @@ import arrow_left from '../../assets/arrow_left.png';
 import arrow_right from '../../assets/arrow_right.png';
 import { useState, useEffect } from 'react';
 
+/**
+ * Carrousel component renders a carousel for displaying a set of pictures.
+ *
+ * @param {Object} props - The properties object.
+ * @param {Array} props.pictures - Array of picture URLs to be displayed in the carousel.
+ * @returns {JSX.Element} The Carrousel component.
+ */
 const Carrousel = ({ pictures }) => {
-    
-    const picturesExtended = [pictures[pictures.length - 1], ...pictures, pictures[0]];
-    
-    const [activeIndex, setActiveIndex] = useState(1);
-    const handlePrev = () => setActiveIndex(prevIndex => prevIndex - 1);
-    const handleNext = () => setActiveIndex(prevIndex => prevIndex + 1);
 
+    const picturesExtended = [pictures[pictures.length - 1], ...pictures, pictures[0]];
+
+    const [activeIndex, setActiveIndex] = useState(1);
     const [transitionEnabled, setTransitionEnabled] = useState(true);
 
-    
+    const handlePrev = () => setActiveIndex(prevIndex => prevIndex - 1);
+     const handleNext = () => setActiveIndex(prevIndex => prevIndex + 1);
 
+    const displayedIndex = activeIndex === 0 ? pictures.length : activeIndex === pictures.length + 1 ? 1 : activeIndex;
 
-
+    // Effect to handle the infinite loop effect by resetting the index without transition
     useEffect(() => {
         const lastIndex = pictures.length;
         const transitionDuration = 500;
@@ -29,6 +35,7 @@ const Carrousel = ({ pictures }) => {
         }
     }, [activeIndex, pictures.length]);
 
+    // Effect to re-enable the transition after it has been temporarily disabled
     useEffect(() => {
         if (!transitionEnabled) {
             const timer = setTimeout(() => {
@@ -37,8 +44,6 @@ const Carrousel = ({ pictures }) => {
             return () => clearTimeout(timer);
         }
     }, [transitionEnabled]);
-
-    const displayedIndex = activeIndex === 0 ? pictures.length : activeIndex === pictures.length + 1 ? 1 : activeIndex;
 
     return (
         <div className="carrousel">
@@ -54,17 +59,17 @@ const Carrousel = ({ pictures }) => {
             >
                 {picturesExtended.map((picture, index) => (
                     <div key={index} className="carrousel__item">
-                        <img src={picture} alt={`Appartement ${index + 1}`} />
+                        <img src={picture} alt={`Appartement ${index}`} />
                     </div>
                 ))}
             </div>
             {pictures.length > 1 && (
                 <div className="carrousel__controls">
-                    <div className="carrousel__arrow carrousel__arrow --left" onClick={handlePrev}>
+                    <div className="carrousel__arrow carrousel__arrow--left" onClick={handlePrev}>
                         <img src={arrow_left} alt="Flèche gauche" />
                     </div>
                     <span className='carrousel__counter'>{displayedIndex}/{pictures.length}</span>
-                    <div className="carrousel__arrow carrousel__arrow --right" onClick={handleNext}>
+                    <div className="carrousel__arrow carrousel__arrow--right" onClick={handleNext}>
                         <img src={arrow_right} alt="Flèche droite" />
                     </div>
                 </div>
